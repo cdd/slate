3d50989688549a8261402ed893bf8ab12a30b3a6
Kelly Joseph Price <kelly@collaborativedrug.com>
2017-10-27 14:36:23 -0700

    Fix clicking after inline files

--- packages/slate-react/src/utils/find-point.js
+++ packages/slate-react/src/utils/find-point.js
@@ -103,6 +103,10 @@ function normalizeNodeAndOffset(node, offset) {
     offset = isLast ? node.textContent.length : 0
   }
 
+  if (node.textContent.includes('\u200B') || node.textContent.includes('\u200A')) {
+    offset = offset - 1
+  }
+
   // Return the node and offset.
   return { node, offset }
 }
